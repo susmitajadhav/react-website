1a363e4c7ae5a3f6dbae6bd128def0d6149361ea
susmitajadhav <45027764+susmitajadhav@users.noreply.github.com>
2025-02-01 12:32:24 +0530

    Updated Product.jsx

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -31,7 +31,7 @@ const ContactPage = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [formData]);
+  }, []);
 
   return (
     <div>
@@ -46,16 +46,28 @@ const ContactPage = () => {
           <div className="contact-page__info" data-aos="fade-right">
             <h2>Our Contact Details</h2>
             <ul>
-              <li><strong>Phone:</strong> 9422225058, 9011731731, 9822760005</li>
-              <li><strong>Directors:</strong></li>
-              <ul>
-                <li><strong>1) Mr. Sudhirr Paatil </strong></li>
-                <li><strong>2) Mr. Vikas Patil </strong></li>
-                <li><strong>3) Mr. Amarsinh Patil </strong></li>
-              </ul>
-              <li><strong>Email:</strong> info@nrfabricator.com / nrdewoo@rediffmail.com</li>
-              <li><strong>Working Hours:</strong> Sun to Fri, 9:00am to 7:00pm</li>
-              <li><strong>Office:</strong> G-58, M.I.D.C, Ahmednagar, Maharashtra - 414111</li>
+              <li>
+                <strong>Phone:</strong> 9422225058, 9011731731, 9822760005
+              </li>
+              <li>
+                <strong>Directors:</strong>
+                <ul>
+                  <li><strong>1) Mr. Sudhirr Paatil</strong></li>
+                  <li><strong>2) Mr. Vikas Patil</strong></li>
+                  <li><strong>3) Mr. Amarsinh Patil</strong></li>
+                </ul>
+              </li>
+              <li>
+                <strong>Email:</strong> info@nrfabricator.com /
+                nrdewoo@rediffmail.com
+              </li>
+              <li>
+                <strong>Working Hours:</strong> Sun to Fri, 9:00am to 7:00pm
+              </li>
+              <li>
+                <strong>Office:</strong> G-58, M.I.D.C, Ahmednagar, Maharashtra
+                - 414111
+              </li>
             </ul>
           </div>
 
@@ -105,7 +117,9 @@ const ContactPage = () => {
                 placeholder="Message"
                 required
               />
-              <button type="submit" className="submit-button">Submit</button>
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
             </form>
           </div>
         </div>
